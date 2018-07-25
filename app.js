130f87ff45c426ae3b2a7f84dd33c1cda2a84686
Diego Rodriguez <diego.r2892@gmail.com>
2018-07-24 21:56:29 -0500

    Implement a download function

--- app.js
+++ app.js
@@ -1,21 +1,65 @@
 var express = require('express');
 var fs = require('fs');
-var request = require('request');
+const axios = require('axios');
 var cheerio = require('cheerio');
 var app     = express();
 
 function getRemoteVersion(url){
-    var re = /Version: \d+.\d/;
-    request(url, function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(html);
+    var re = /Version: \d+.\d+/;
+    return new Promise((resolve, reject)=>{
+        axios.get(url)
+        .then((res)=>{
+            var $ = cheerio.load(res.data);
             let version = $.text().match(re)
-            console.log(version[0].split(' ')[1]) //
-        }
-    }); 
+            resolve(version[0].split(' ')[1])
+        })
+        .catch(function (error) {
+            reject(error)
+        })
+    })
+    
+}
+
+async function downloadFile(url,name,addonVersion){
+    if (name === 'elvui'){
+        let version = await getRemoteVersion(addonVersion)
+        console.log(url+name+'-'+version+'.zip')
+        axios({
+            url: url+name+'-'+version+'.zip',
+            method: 'GET',
+            responseType: 'arraybuffer' // important
+        })
+        .then((response) => {
+            fs.writeFileSync(name+'-'+version+'.zip', response.data)
+            return name+'-'+version+'.zip'
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }
+    else{
+        let version = await getRemoteVersion(addonVersion)
+        axios({
+            url: url,
+            method: 'GET',
+            responseType: 'arraybuffer' // important
+        })
+        .then((response) => {
+            fs.writeFileSync(name+'-'+version+'.zip', response.data)
+            return name+'-'+version+'.zip'
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }
+    
 }
 
 
-getRemoteVersion('https://git.tukui.org/elvui/elvui/raw/master/ElvUI/ElvUI.toc')
-getRemoteVersion('https://git.tukui.org/Azilroka/AddOnSkins/raw/master/AddOnSkins.toc')
-getRemoteVersion('https://git.tukui.org/Darth_Predator/elvui-shadowandlight/raw/master/ElvUI_SLE/ElvUI_SLE.toc')
+
+
+downloadFile('https://www.tukui.org/downloads/','elvui','https://git.tukui.org/elvui/elvui/raw/master/ElvUI/ElvUI.toc')
+downloadFile('https://www.tukui.org/addons.php?download=3','AddonSkins', 'https://git.tukui.org/Azilroka/AddOnSkins/raw/master/AddOnSkins.toc')
+downloadFile('https://www.tukui.org/addons.php?download=38','SEL', 'https://git.tukui.org/Darth_Predator/elvui-shadowandlight/raw/master/ElvUI_SLE/ElvUI_SLE.toc')
